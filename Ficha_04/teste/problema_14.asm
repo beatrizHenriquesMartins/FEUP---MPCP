7cb9d8b77ea642496e66193d11f220226a348eb2
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-04-17 12:24:18 +0100

    exercicio 14 - completo

--- Ficha_04/teste/problema_14.asm
+++ Ficha_04/teste/problema_14.asm
@@ -7,11 +7,19 @@ rotmax proto address: DWORD, seqsize: DWORD
 .data
 	seq SDWORD 1, 2, -3, -10, 50, -20
     msg1 BYTE "Valor máximo: %d", 10, 13, 0
+    msg2 BYTE "Valor médio: %d", 10, 13, 0
+    msg3 BYTE "Valor médio com overflow: %d", 10, 13, 0
 
 ;; seccao de codigo principal 
 .code
     main PROC C
         invoke rotmax, offset seq, lengthof seq
+        invoke printf, offset msg1, EAX
+
+        invoke rotmed, offset seq, lengthof seq
+        invoke printf, offset msg1, EAX
+
+        invoke rotmedOverflow, offset seq, lengthof seq
         invoke printf, offset msg1, EAX
 
         invoke _getch
@@ -21,6 +29,7 @@ rotmax proto address: DWORD, seqsize: DWORD
     main ENDP
 
     ;; --------------Exercicio 14--------------
+    ;; calculo do maximo da sequencia
     rotmax proc uses ESI address: DWORD, seqsize: DWORD
                 mov ECX, seqsize
                 mov ESI, address
@@ -34,17 +43,35 @@ rotmax proto address: DWORD, seqsize: DWORD
                 ret
     rotmax ENDP
 
-
-    rotmed proc uses ESI seq_addr: DWORD, seq_size:DWORD
+    ;; calculo da média, assumindo que não existe overflow
+    rotmed proc uses ESI ECX EDX seq_addr: DWORD, seq_size:DWORD
                 mov ECX, seq_size
                 mov ESI, seq_addr
                 xor EAX, EAX
                 mov EAX, [ESI]
-                add ESI, 4
                 dec ECX
-        soma:   add EAX, [ESI]
+        soma:   add ESI, 4
+                add EAX, [ESI]
                 loop
-                idiv EAX, ECX
+                mov ECX, seq_size
+                xor EDX, EDX
+                idiv ECX
                 ret
     rotmed ENDP
+
+    ;; calculo da média, assumindo que existe overflow
+    rotmedOverflow proc uses ESI ECX EDX seq_addr: DWORD, seq_size:DWORD
+                mov ECX, seq_size
+                mov ESI, seq_addr
+                xor EAX, EAX
+                xor EDX, EDX
+        soma:   add EAX, [ESI]
+                jno segue
+                inc EDX
+        segue:  add ESI, 4
+                loop soma
+                mov ecx, seq_size
+                idiv ECX
+                ret        
+    rotmedOverflow ENDP
 end 
